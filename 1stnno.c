62f22ab2283d0bea9375527f7af914143b3215ac
Rishi Mehta <rishimehta0223@gmail.com>
2026-09-16 23:47:24 +0530

    Add files via upload

---
+++ 1stnno.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{//sum of the first n natural numbers
+  int a,b;
+  printf("enter n th no.:");
+  scanf("%i",&a);
+  b=(a*(a+1))/2;
+  printf("enter the value of n and the sum of 1st n natural no. are :%i\n",b);
+return 0;
+}
+
