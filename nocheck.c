0338fa428e55170e295b2b34a21e2b27b3a68286
Rishi Mehta <rishimehta0223@gmail.com>
2026-09-17 23:58:01 +0530

    Add files via upload

---
+++ nocheck.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{//input an integer and check whether it is even or odd using if–else.
+  int a;
+  printf("enter a integer :");
+  scanf("%i",&a);
+  if (a%2==0)
+  {
+    printf("%i its an even interger\n");
+  }
+  else
+  {
+    printf("%i its a odd interger\n");
+  }
+return 0;
+}
