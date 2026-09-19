62ed1a51c9bc6023a0719a0c7a99a4e36666aa98
Rishi Mehta <rishimehta0223@gmail.com>
2026-09-20 00:01:46 +0530

    Add files via upload

---
+++ calc.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main()
+{//program to implement a basic calculator using switch-case for +, -, *, /, %
+  double a,b,d;
+  char c;
+  printf("enter 1st no. :");
+  scanf("%lf",&a);
+  printf("enter 2nd no. :");
+  scanf("%lf",&b);
+  printf("enter what u want to do with no. (symbol) :");
+  scanf(" %c",&c);
+  switch (c)
+  {
+    case '+':
+      d=a+b;
+      printf("u have selected + %f\n",d);
+      break;
+    case '-':
+      d=a-b;
+      printf("u have selected - %f\n",d);
+      break;
+    case '*':
+      d=a*b;
+      printf("u have selected * %f\n",d);
+      break;
+    case '/':
+      d=a/b;
+      printf("u have selected / %f\n",d);
+      break;
+    case '%':
+      d=(int)a%(int)b;
+      printf("u have selected %% %f\n",d);
+      break;
+    default:
+      printf("enter valid symbol\n");
+  }
+return 0;
+}
