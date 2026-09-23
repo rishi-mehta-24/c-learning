e61cab428fb0779495c47eb02d6ab6e7545dedf2
Rishi Mehta <rishimehta0223@gmail.com>
2026-09-24 00:12:17 +0530

    Add files via upload

---
+++ 26-1ton.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{//program to print numbers from 1 to n
+  int n;
+  printf("enter range :");
+  scanf("%d",&n);
+  for (int i=1;i<=n;i++)
+  {
+    printf("%d\n",i);
+  }
+return 0;
+}
